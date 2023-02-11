a50cfb04ebf3be06c37efb9ca80eb122a1498de4
Radha_Singh <radha061btcsai20@igdtuw.ac.in>
2023-02-11 09:19:26 +0530

    ui updated

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
+import joblib
 
 # creating flask object
 app = Flask(__name__)
 
 
 model = joblib.load("./random_forest.joblib")
-@app.route('/', methods=['GET', 'POST'])
+
 def Predict(state,district,year,month,price,soil):
     y_pred = model.predict(state,district,year,month,price,soil)
+    print(y_pred)
     return y_pred
 
 
 @app.route('/', methods=['GET', 'POST'])
 def get_val():
     if (request.method == 'POST'):
+        state = request.form.get('state')
+        district = request.form.get('district')
+        year = request.form.get('year')
+        month = request.form.get('month')
+        price = request.form.get('price')
+        soil = request.form.get('soil')  
+        return render_template('crop.html', prediction=Predict(state, district, year, month, price, soil) )
 
-        if request.form["Submit_B"]=="newsContent":
-            text = request.form.get('content')
-            return render_template('Index.html', prediction=Predict(text) , scroll="detection")
+    return render_template('crop.html')
 
-    return render_template('Index.html')
+if __name__=='__main__':
+    app.run()
